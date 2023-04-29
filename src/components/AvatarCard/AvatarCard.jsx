7dc7de1032b312f3a9322c4b2cd68665e60007e2
samssjubair <samssjubair@gmail.com>
2023-04-29 23:15:31 +0600

    basic layout of landing page done

--- src/components/AvatarCard/AvatarCard.jsx
+++ src/components/AvatarCard/AvatarCard.jsx
@@ -1,33 +1,63 @@
 import React from 'react';
-import ava from '../../assets/images/X(F).png'
+import ava from '../../assets/images/A.png'
 import { AiOutlineHeart } from "react-icons/ai";
 import { HiOutlineArrowUpOnSquare } from "react-icons/hi2";
 import { BsCartPlus } from "react-icons/bs";
 import './AvatarCard.css'
+import ReactStars from "react-rating-stars-component";
 
-const AvatarCard = () => {
+
+const AvatarCard = ({data}) => {
+    const imagePath = "/src/assets/images/" + data.image;
+
+    const ratingChanged = (newRating) => {
+        console.log(newRating);
+      };
+    
     return (
-        <div className='col-md-3 p-1'>
+        <div className='col-lg-3 col-md-6 col-sm-12 p-1'>
             <div className="card border-0">
-            <img src={ava} className="card-img-top rounded" alt="avatar"/>
+            <img src={imagePath} className="card-img-top rounded" alt="avatar"/>
             <button className="btn btn-sm btn-primary" style={{position: 'absolute', top: 5, right: 5}}><BsCartPlus/> Add</button>
             <div className="card-body">
-                <h5 className="card-title">Avatar Name</h5>
+                <h5 className="card-title">{data.name}</h5>
                 <div className="row">
-                <div className="col-10">
-                    <p className="card-text">Ratings</p>
+                <div className="col-10 d-flex">
+                
+                    <ReactStars
+                        count={5}
+                        value={data.rating}
+                        isHalf={true}
+                        onChange={ratingChanged}
+                        size={18}
+                        activeColor="#ffd700"
+                    />
+                
+                    <p className="card-text">{data.rating} & {data.likes} likes</p>
                 </div>
                 <div className="col-2">
-                    <button className="btn btn-sm heart-btn">
+                    <button className="btn btn-sm heart-btn pt-0">
                         <AiOutlineHeart style={{fontSize: '1.5rem'}}/>
                     </button>
                 </div>
                 </div>
-                <p className="card-text">Price</p>
-                <p className="card-text">Short description</p>
-                <button className="btn btn-sm pe-0 float-end">
-                    <HiOutlineArrowUpOnSquare style={{fontSize: '2rem'}}/>
-                </button>
+                <div>
+                    <img src={ava} alt="avatar" className='rounded-circle ava-creator me-1'/>
+                    <span className='creator-name'>Avatar Joe's</span>
+                </div>
+                
+                <p className="card-text fs-4 mb-1"><small>$</small>{data.price}</p>
+                <div className='d-flex'>
+                    <div className='content-indicator'>
+                    </div>
+                    <p>PC Only</p>
+                </div>
+                <div className='d-flex'>
+                    <p className="card-text">{data.description}</p>
+                    <button className="btn btn-sm pe-0 float-end">
+                        <HiOutlineArrowUpOnSquare style={{fontSize: '2rem'}}/>
+                    </button>
+                </div>
             </div>
             </div>
 
--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AvatarCard from '../AvatarCard/AvatarCard';
+import Pagination from 'react-bootstrap/Pagination';
+import jsonData from './../../assets/data.json'
 
 const MainContainer = () => {
-    return (
-        <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
-         <div className="row">
-            <AvatarCard/>
-            <AvatarCard/>
-            <AvatarCard/>
-            <AvatarCard/>
-            <AvatarCard/>
-            <AvatarCard/>
-         </div>
-        </main>
-    );
+    console.log(jsonData)
+  const [activePage, setActivePage] = useState(1);
+  const cardsPerPage = 12;
+  const totalCards = jsonData.avatars.length;
+  const totalPages = Math.ceil(totalCards / cardsPerPage);
+  const cardIndexStart = (activePage - 1) * cardsPerPage;
+  const cardIndexEnd = Math.min(activePage * cardsPerPage, totalCards);
+
+  const renderAvatarCards = () => {
+    const avatarCards = [];
+    for (let i = cardIndexStart; i < cardIndexEnd; i++) {
+      const data = jsonData.avatars[i];
+      avatarCards.push(<AvatarCard key={i} data={data} />);
+    }
+    return avatarCards;
+  };
+
+  const handlePageChange = (page) => {
+    setActivePage(page);
+  };
+
+  const renderPaginationItems = () => {
+    const paginationItems = [];
+    for (let i = 1; i <= totalPages; i++) {
+      paginationItems.push(
+        <Pagination.Item key={i} active={i === activePage} onClick={() => handlePageChange(i)}>
+          {i}
+        </Pagination.Item>
+      );
+    }
+    return paginationItems;
+  };
+
+  return (
+    <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
+      <div className="row">
+        {renderAvatarCards()}
+      </div>
+      <div className="d-flex justify-content-center">
+        <Pagination>{renderPaginationItems()}</Pagination>
+      </div>
+    </main>
+  );
 };
 
 export default MainContainer;
